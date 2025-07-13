2503872a8c88f4b1881522b2bb52d325d9771c75
Adriel-747 <adrielgomes747@gmail.com>
2025-07-13 14:47:50 -0300

    Alteração Geral do Projeto ( Exclusão Back)

--- backEnd/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-router.post('/criar', userController.criarUsuario);
-router.post('/resultado', userController.adicionarResultado);
-router.get('/historico/:id', userController.buscarHistorico);
-
-module.exports = router;
